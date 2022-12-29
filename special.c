84e21e6bbf7d6e996d78118f4d86b33bb0f467ef
Timon van der Berg <tmnvanderberg@gmail.com>
2022-12-29 20:30:34 +0100

    ch2: special chars
    Signed-off-by: Timon van der Berg <tmnvanderberg@gmail.com>

---
+++ special.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  printf("Hello WW\borld\n");
+  printf("Hello \v World\n");
+  printf("Ring dong \a\n");
+  printf("before\r after\n");
+  printf("first \f second \f third \f last \n");
+}
